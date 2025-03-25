176d35628b8263c007fd3d51bb50c3230060e3a6
harshjf <harsh@juniperforge.com>
2025-03-25 13:35:12 +0530

    Testing changes for CORS error

--- src/server.ts
+++ src/server.ts
@@ -26,8 +26,11 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://localhost:3001",
+    "http://147.93.111.144",
     "https://tutorioplus.com",
+    "https://tutorioplus.com/",
     "https://admin.tutorioplus.com",
+    "https://admin.tutorioplus.com/",
   ] /* env.CORS_ORIGIN */,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
